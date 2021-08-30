5bc0c2fca014f03481e86b5c4bdc953d18365dfa
Yama <yama.spec@yahoo.es>
2021-08-30 14:14:56 +1000

    End of section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,34 +1,34 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using API.Data;
-using API.Entities;
+using API.DTOs;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class UsersController : BaseApiController
-    {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
-        {
-            _context = context;
+	[Authorize]
+	public class UsersController : BaseApiController
+	{
+		private readonly IUserRepository _userRepository;
+		private readonly IMapper _mapper;
+		public UsersController(IUserRepository userRepository, IMapper mapper)
+		{
+			_userRepository = userRepository;
+			_mapper = mapper;
+		}
 
-        }
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+		{
+			return Ok(await _userRepository.GetMembersAsync());
+		}
 
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-        {
-            return await _context.Users.ToListAsync();
-        }
-
-        [Authorize]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
-        {
-            return await _context.Users.FindAsync(id);
-        }
-    }
+		[HttpGet("{username}")]
+		public async Task<ActionResult<MemberDto>> GetUser(string username)
+		{
+			return await _userRepository.GetMemberAsync(username);
+		}
+	}
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace API
 {
-    public class Program
+	public class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            // using var scope = host.Services.CreateScope();
+            // var services = scope.ServiceProvider;
+            // try
+            // {
+            //     var context = services.GetRequiredService<DataContext>();
+            //     await context.Database.MigrateAsync();
+            //     await Seed.SeedUsers(context);
+            // }
+            // catch (Exception ex)
+            // {
+            //     var logger = services.GetRequiredService<ILogger<Program>>();
+            //     logger.LogError(ex, "An error occurred");
+            // }
+            // await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
